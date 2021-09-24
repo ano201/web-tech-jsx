fe1985a65ac08fcecb579a4a2d405462e2423bba
ano201 <ahmu689@gmail.com>
2021-09-25 03:22:19 +0600

    fix

--- src/component/Home.js
+++ src/component/Home.js
@@ -9,14 +9,14 @@ const Home = ()=> {
   <div className='container'>
    <sections className='d-flex align-items-center mt-lg-3'>
      <div className='row'>
-      <div className='col-md-6 pt-5 pt-lg-0 order-1 Order-sm-2'>
+      <div className='col-md-6 pt-5 pt-lg-0 order-2 order-md-1'>
        <h1>Welcome to <strong className='pulse'>Web Tech</strong></h1>
        <h3 className='my-3'>We are the team of creative developer making awesome websites.</h3>
        <div className='mt-3'>
         <NavLink to='/service' className='btn btn-outline-info rounded-pill fs-md-3 px-4 py-2'>Get Started</NavLink>
        </div>
       </div>
-      <div className='col-md-6 order-2 Order-sm-2 header-img'>
+      <div className='col-md-6 order-1 order-md-2 header-img'>
        <img src={webTech} className='img-fluid pulse' alt='' />
       </div>
      </div>
